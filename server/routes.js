f52810504d6ce2a2100f777de16061574e100d73
Don Smith <don@antiantonym.com>
2019-04-25 18:25:29 +1200

    add tests for server/routes

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,10 @@
 const hash = require('./hash')
 const token = require('./token')
+const { registerUrl, signInUrl } = require('../endpoints')
+
+const DATABASE_ERROR = 'DATABASE_ERROR'
+const INVALID_CREDENTIALS = 'INVALID_CREDENTIALS'
+const USERNAME_UNAVAILABLE = 'USERNAME_UNAVAILABLE'
 
 module.exports = {
   applyAuthRoutes
@@ -9,23 +14,23 @@ module.exports = {
 function applyAuthRoutes (router, functions) {
   const issueToken = token.getIssuer(functions.getUserByName)
 
-  router.post('/register', register, issueToken)
-  router.post('/signin', signIn, issueToken)
+  router.post(registerUrl, register, issueToken)
+  router.post(signInUrl, signIn, issueToken)
 
   function register (req, res, next) {
     functions.userExists(req.body.username)
       .then(exists => {
         if (exists) {
           return res.status(400).send({
-            errorType: 'USERNAME_UNAVAILABLE'
+            errorType: USERNAME_UNAVAILABLE
           })
         }
         functions.createUser(req.body.username, req.body.password)
           .then(() => next())
       })
       .catch(() => {
-        res.status(400).send({
-          errorType: 'DATABASE_ERROR'
+        res.status(500).send({
+          errorType: DATABASE_ERROR
         })
       })
   }
@@ -33,24 +38,26 @@ function applyAuthRoutes (router, functions) {
   function signIn (req, res, next) {
     functions.getUserByName(req.body.username)
       .then(user => {
-        return user || invalidCredentials(res)
+        if (user) return user
+        throw new Error(INVALID_CREDENTIALS)
       })
       .then(user => {
         return user && hash.verify(user.hash, req.body.password)
       })
       .then(isValid => {
-        return isValid ? next() : invalidCredentials(res)
+        if (isValid) return next()
+        throw new Error(INVALID_CREDENTIALS)
       })
-      .catch(() => {
-        res.status(400).send({
-          errorType: 'DATABASE_ERROR'
+      .catch(err => {
+        if (err.message === INVALID_CREDENTIALS) {
+          return res.status(400).send({
+            errorType: INVALID_CREDENTIALS
+          })
+        }
+
+        res.status(500).send({
+          errorType: DATABASE_ERROR
         })
       })
-  }
-
-  function invalidCredentials (res) {
-    res.status(400).send({
-      errorType: 'INVALID_CREDENTIALS'
-    })
   }
 }
---
+++ tests/server/routes.test.js
@@ -0,0 +1,159 @@
+const express = require('express')
+const request = require('supertest')
+
+const endpoints = require('../../endpoints')
+
+const functions = {
+  getUserByName: () => Promise.resolve({
+    id: 1,
+    hash: 'test-hash',
+    username: 'test-user'
+  }),
+  userExists: () => Promise.resolve(false),
+  createUser: () => Promise.resolve()
+}
+
+process.env.JWT_SECRET = 'test-jwt-secret'
+
+describe('applyAuthRoutes', () => {
+  beforeEach(() => jest.resetModules())
+
+  it('creates an operational /auth/register route', () => {
+    const { applyAuthRoutes } = require('../../server/routes')
+
+    const server = express()
+    server.use(express.json())
+    const router = express.Router()
+    server.use('/', router)
+
+    applyAuthRoutes(router, functions)
+
+    return request(server)
+      .post(endpoints.registerUrl)
+      .expect(200)
+      .send({
+        username: 'test-user',
+        password: 'test-password'
+      })
+      .then(res => {
+        expect(res.body.message).toMatch('successful')
+        expect(res.body.token.length).toBe(168)
+      })
+  })
+
+  it('creates an operational /auth/signin route', () => {
+    jest.mock('../../server/hash', () => {
+      return {
+        verify: () => Promise.resolve(true)
+      }
+    })
+    const { applyAuthRoutes } = require('../../server/routes')
+
+    const server = express()
+    server.use(express.json())
+    const router = express.Router()
+    server.use('/', router)
+
+    applyAuthRoutes(router, functions)
+
+    return request(server)
+      .post(endpoints.signInUrl)
+      .expect(200)
+      .send({
+        username: 'test-user',
+        password: 'test-password'
+      })
+      .then(res => {
+        expect(res.body.message).toMatch('successful')
+        expect(res.body.token.length).toBe(168)
+      })
+  })
+
+  describe('/auth/register', () => {
+    it('returns status 400 if the username is already being used', () => {
+      const { applyAuthRoutes } = require('../../server/routes')
+
+      const server = express()
+      server.use(express.json())
+      const router = express.Router()
+      server.use('/', router)
+
+      functions.userExists = () => Promise.resolve(true)
+      applyAuthRoutes(router, functions)
+
+      return request(server)
+        .post(endpoints.registerUrl)
+        .expect(400)
+        .send({})
+        .then(res => {
+          expect(res.body.errorType).toMatch('USERNAME_UNAVAILABLE')
+        })
+    })
+
+    it('returns status 500 if there is a database error', () => {
+      const { applyAuthRoutes } = require('../../server/routes')
+
+      const server = express()
+      server.use(express.json())
+      const router = express.Router()
+      server.use('/', router)
+
+      functions.userExists = () => Promise.reject(new Error())
+      applyAuthRoutes(router, functions)
+
+      return request(server)
+        .post(endpoints.registerUrl)
+        .expect(500)
+        .send({})
+        .then(res => {
+          expect(res.body.errorType).toMatch('DATABASE_ERROR')
+        })
+    })
+  })
+
+  describe('/auth/signin', () => {
+    it('returns a status 400 if no user is returned from the database', () => {
+      const { applyAuthRoutes } = require('../../server/routes')
+
+      const server = express()
+      server.use(express.json())
+      const router = express.Router()
+      server.use('/', router)
+
+      functions.getUserByName = () => Promise.resolve(null)
+      applyAuthRoutes(router, functions)
+
+      return request(server)
+        .post(endpoints.signInUrl)
+        .expect(400)
+        .send({})
+        .then(res => {
+          expect(res.body.errorType).toMatch('INVALID_CREDENTIALS')
+        })
+    })
+
+    it('returns a status 400 if the password and hash do not match', () => {
+      jest.mock('../../server/hash', () => {
+        return {
+          verify: () => Promise.resolve(false)
+        }
+      })
+      const { applyAuthRoutes } = require('../../server/routes')
+
+      const server = express()
+      server.use(express.json())
+      const router = express.Router()
+      server.use('/', router)
+
+      applyAuthRoutes(router, functions)
+
+      return request(server)
+        .post(endpoints.signInUrl)
+        .expect(400)
+        .send({})
+        .then(res => {
+          expect(res.body.errorType).toMatch('INVALID_CREDENTIALS')
+        })
+    })
+  })
+})
