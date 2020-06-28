4074e59ee0c11d6b90420c796845c1931a9c6f78
Don Smith <hi@dons.dev>
2020-06-28 21:03:00 +1200

    remove tutorial

--- tutorial/server/index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const server = require('./server')
-
-const port = 3030
-
-server.listen(port, () => {
-  // eslint-disable-next-line no-console
-  console.log('Server listening on port', port)
-})
